c91fe58bcc33824694e7401c5d2fd996980f5e51
Henna Singh <hennasingh04@gmail.com>
2023-11-08 22:09:03 +0000

    added data attribute to copy color to clipboard

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const matches = container.querySelectorAll('div.color')
 const hexMatches = container.querySelectorAll('div.hex > p')
 const generateBtn = document.getElementById('generate-scheme')
 
+
+document.addEventListener('click', function(event) {
+    if(event.target.dataset.color) {
+        if(navigator.clipboard){
+            const colorHex = event.target.dataset.color
+            navigator.clipboard.writeText(colorHex)
+        }
+    }
+})
 generateBtn.addEventListener('click', generateScheme)
 
 function generateScheme() {
@@ -23,7 +32,10 @@ function getColors(seedColor, mode){
     .then(data => {
         data.colors.forEach((color, index) => {
             matches[index].style.backgroundColor = color.hex.value
+            matches[index].setAttribute('data-color',color.hex.value)
+
             hexMatches[index].textContent = color.hex.value
+            hexMatches[index].setAttribute('data-color', color.hex.value)
         });
     })
 }
