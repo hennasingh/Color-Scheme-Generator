9eaaf82f4917cb6a65bab2f085d4cc44a67a25a2
Henna Singh <hennasingh04@gmail.com>
2024-04-02 14:25:58 +0530

    simplified grid, changed order of functions, edited naming conventions and added copy animation

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
+const container = document.querySelector("div.color-output-container");
+const matches = container.querySelectorAll("div.color");
+const hexMatches = container.querySelectorAll("div.hex-values");
+const generateBtn = document.getElementById("generate-scheme");
 
-const container  = document.querySelector("div.color-output")
-const matches = container.querySelectorAll('div.color')
-const hexMatches = container.querySelectorAll('div.hex > p')
-const generateBtn = document.getElementById('generate-scheme')
+/** Copy hex code and create animation when copying **/
 
+document.addEventListener("click", function (event) {
+  if (event.target.dataset.color) {
+    copyColorCode(event.target.dataset.color);
+    handleCopyAnimation(event.target);
+  }
+});
 
-document.addEventListener('click', function(event) {
-    if(event.target.dataset.color) {
-        if(navigator.clipboard){
-            const colorHex = event.target.dataset.color
-            navigator.clipboard.writeText(colorHex)
-        }
-    }
-})
-generateBtn.addEventListener('click', generateScheme)
-
-function generateScheme() {
-    let seedColor = document.getElementById('color').value
-    seedColor = seedColor.slice(1)
-    const mode = document.getElementById('color-modes').value
-    getColors(seedColor, mode)
+function copyColorCode(hexCode) {
+  console.log("Copy!");
+  navigator.clipboard.writeText(hexCode);
 }
+
+function handleCopyAnimation(targetColor) {
+  console.log(targetColor);
+  matches.forEach((item) => {
+    item.classList.remove("copied");
+  });
+  hexMatches.forEach((item) => {
+    item.classList.remove("copied");
+  });
+
+  targetColor.classList.add("copied");
+
+  setTimeout(() => {
+    targetColor.classList.remove("copied");
+  }, 250);
+}
+
+generateBtn.addEventListener("click", generateScheme);
+
+/** Get initial color scheme */
+
+init();
 
 function init() {
-    getColors('F55A5A', 'monochrome')
+  getColors("F55A5A", "monochrome");
 }
 
-function getColors(seedColor, mode){
-    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}&count=5`)
-    .then(res => res.json())
-    .then(data => {
-        data.colors.forEach((color, index) => {
-            matches[index].style.backgroundColor = color.hex.value
-            matches[index].setAttribute('data-color',color.hex.value)
+function getColors(seedColor, mode) {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      data.colors.forEach((color, index) => {
+        matches[index].style.backgroundColor = color.hex.value;
+        matches[index].setAttribute("data-color", color.hex.value);
 
-            hexMatches[index].textContent = color.hex.value
-            hexMatches[index].setAttribute('data-color', color.hex.value)
-        });
-    })
+        hexMatches[index].textContent = color.hex.value;
+        hexMatches[index].setAttribute("data-color", color.hex.value);
+      });
+    });
 }
 
-init()
+/** Generate new color scheme on button click */
+function generateScheme() {
+  const seedColor = document.getElementById("color").value.slice(1);
+
+  const mode = document.getElementById("color-modes").value;
+  getColors(seedColor, mode);
+}
